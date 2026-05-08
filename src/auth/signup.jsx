08a7e94e079e1acf7f7e98f5d4fffc3cc3a2e468
Ogaga Hopeson Tega <hopesonogaga@gmail.com>
2026-05-08 09:36:10 +0100

    ui patch

--- src/auth/signup.jsx
+++ src/auth/signup.jsx
@@ -213,14 +213,14 @@ export default function Signup() {
                     <div className="w-full border-t border-gray-200"></div>
                   </div>
 
-                  <div className="relative flex justify-center text-sm font-medium leading-6">
+                  {/* <div className="relative flex justify-center text-sm font-medium leading-6">
                     <span className="bg-white px-4 text-gray-400">
                       Or continue with
                     </span>
-                  </div>
+                  </div> */}
                 </div>
 
-                {/* Google Button */}
+                {/* Google Button
                 <div className="mt-6">
                   <button
                     type="button"
@@ -250,7 +250,7 @@ export default function Signup() {
                     </svg>
                     Continue with Google
                   </button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
